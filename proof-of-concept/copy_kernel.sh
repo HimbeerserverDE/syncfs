d939cd157584cfc9d6fc319ea2bf8fd6015f6b8c
HimbeerserverDE <himbeerserverde@gmail.com>
2022-08-22 12:27:24 +0200

    relink existing kernel+initrd

--- proof-of-concept/copy_kernel.sh
+++ proof-of-concept/copy_kernel.sh
@@ -5,5 +5,8 @@ KERNELVER=$(uname -r)
 cp /boot/vmlinuz-${KERNELVER} /var/lib/tftp/cloner/boot/
 cp /boot/initrd.img-${KERNELVER} /var/lib/tftp/cloner/boot/
 
+rm /var/lib/tftp/cloner/vmlinuz
+rm /var/lib/tftp/cloner/initrd.img
+
 ln -s /var/lib/tftp/cloner/boot/vmlinuz-${KERNELVER} /var/lib/tftp/cloner/vmlinuz
 ln -s /var/lib/tftp/cloner/boot/initrd.img-${KERNELVER} /var/lib/tftp/cloner/initrd.img
